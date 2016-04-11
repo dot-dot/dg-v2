c9deee8eeeb42bde3cf532c0cecddb8b583bb1c9
dotdotdot <anton@dotdotdot.com.au>
2016-04-12 00:52:48 +1000

    initial commit

---
+++ src/dg-v2/Controllers/CatFactsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using dg.Models;
+using Flurl;
+using Flurl.Http;
+using Microsoft.AspNet.Mvc;
+
+namespace dg.Controllers
+{
+    [Route("api/[controller]")]
+    public class CatFactsController :Controller
+    {
+        // GET: api/catfacts
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            try
+            {
+                var facts = await "http://catfacts-api.appspot.com"
+                    .AppendPathSegment("api/facts")
+                    .GetJsonAsync<Fact>();
+
+                return facts.facts.FirstOrDefault();
+            }
+            catch (Exception)
+            {
+
+                return "meow";
+            }            
+        }
+    }
+}
---
+++ src/dg-v2/Models/Fact.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace dg.Models
+{
+    public class Fact
+    {
+        public IEnumerable<string> facts { get; set; }
+        public bool success { get; set; }
+
+    }
+}
