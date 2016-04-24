924aaf7e2e1ae9f40408aaaa4bfce1192c4d08aa
dotdotdot <anton@dotdotdot.com.au>
2016-04-24 21:58:49 +1000

    get api now return multiple facts and buffered on front end attempt to enable cors for asp.net core :(

--- src/dg-v2/Controllers/CatFactsController.cs
+++ src/dg-v2/Controllers/CatFactsController.cs
@@ -1,32 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dg.Models;
 using Flurl;
 using Flurl.Http;
+using Microsoft.AspNet.Cors;
 using Microsoft.AspNet.Mvc;
 
 namespace dg.Controllers
-{
+{    
+    [EnableCors("AllowAll")]
     [Route("api/[controller]")]
     public class CatFactsController :Controller
     {
+
         // GET: api/catfacts
         [HttpGet]
-        public async Task<string> Get()
+        [RouteAttribute("{count?}")]
+        public async Task<IEnumerable<string>> Get(int count = 1)
         {
             try
             {
                 var facts = await "http://catfacts-api.appspot.com"
                     .AppendPathSegment("api/facts")
-                    .GetJsonAsync<Fact>();
+                    .SetQueryParams(new 
+                {
+                    number = count
+                })
+            .GetJsonAsync<Fact>();
 
-                return facts.facts.FirstOrDefault();
+                return facts.facts;
             }
             catch (Exception)
             {
 
-                return "meow";
+                return new [] { "meow"};
             }            
         }
     }
--- src/dg-v2/Startup.cs
+++ src/dg-v2/Startup.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Cors.Infrastructure;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Cors;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+
 
 namespace dg
 {
@@ -22,8 +27,32 @@ namespace dg
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+           
+
+           
+
             // Add framework services.
             services.AddMvc();
+
+            services.AddCors();
+            services.Configure<CorsOptions>(options => options.AddPolicy("AllowAll",
+                o => o.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()));
+
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("AllowAll",
+            //        o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            //});
+
+
+            //services.Configure<MvcOptions>(options =>
+            //{
+            //    options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAll"));
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +67,12 @@ namespace dg
 
             app.UseStaticFiles();
 
+
+            //app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            
             app.UseMvc();
+
+            app.UseCors("AllowAll");
         }
 
         // Entry point for the application.
